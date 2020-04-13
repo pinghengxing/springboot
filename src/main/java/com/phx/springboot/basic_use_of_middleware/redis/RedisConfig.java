e086206219a6ebbc19e73eafa0e7c58ad65ec2ac
pinghengxing <a454902046@163.com>
2020-04-13 23:20:28 +0800

    1

--- src/main/java/com/phx/springboot/basic_use_of_middleware/redis/RedisConfig.java
+++ src/main/java/com/phx/springboot/basic_use_of_middleware/redis/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -16,8 +17,17 @@ public class RedisConfig {
 
     
 
+//    @Bean
+//    public RedisTemplate<String,Object> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory){
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        template.setConnectionFactory(redisConnectionFactory);
+//        return template;
+//    }
+
     @Bean
-    public RedisTemplate<String,Object> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory){
+    public RedisTemplate<String,Object> redisCacheTemplate(RedisConnectionFactory redisConnectionFactory){
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
